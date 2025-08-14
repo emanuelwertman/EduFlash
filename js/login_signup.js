ac504eb2e1f4e4d6ffccfa5947e7119e956c636a
ManoW3 <manoisnotcool@gmail.com>
2025-08-13 23:09:01 -0400

    Made register/login changing work

--- js/login_signup.js
+++ js/login_signup.js
@@ -1,12 +1,17 @@
-function showRegister() {
-	document.getElementById("loginForm").style.display = "none";
-	document.getElementById("registerForm").style.display = "block";
-}
+const registerButton = document.getElementById("register");
 
-function showLogin() {
+registerButton.addEventListener('click', function() {
+	document.getElementById("registerForm").style.display = "block";
+    document.getElementById("loginForm").style.display = "none";
+});
+
+
+const loginButton = document.getElementById("login");
+
+loginButton.addEventListener('click', function() {
 	document.getElementById("registerForm").style.display = "none";
-	document.getElementById("loginForm").style.display = "block";
-}
+    document.getElementById("loginForm").style.display = "block";
+});
 
 function valid() {
 	const password = document.getElementById("pass").value;
