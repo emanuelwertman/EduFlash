5ad96a249ac6c4e14c157c78cab8452895a1843b
Pranav-P26 <pramod.pranav26@gmail.com>
2025-08-15 08:35:24 -0400

    Fixed join button on navbar functionality

--- static/js/login_signup.js
+++ static/js/login_signup.js
@@ -1,32 +1,43 @@
-const registerButton = document.getElementById("register");
+const showForm = (mode) => {
+  const registerForm = document.getElementById("registerForm");
+  const loginForm = document.getElementById("loginForm");
 
-registerButton.addEventListener('click', function() {
-	document.getElementById("registerForm").style.display = "block";
-    document.getElementById("loginForm").style.display = "none";
+  registerForm.style.display = mode === "register" ? "block" : "none";
+  loginForm.style.display = mode === "login" ? "block" : "none";
+};
+
+document.addEventListener("click", (e) => {
+  const id = e.target && e.target.id;
+  if (id === "register") showForm("register");
+  else if (id === "login") showForm("login");
 });
 
-
-const loginButton = document.getElementById("login");
-
-loginButton.addEventListener('click', function() {
-	document.getElementById("registerForm").style.display = "none";
-    document.getElementById("loginForm").style.display = "block";
-});
+(function checkMode() {
+  const hash = location.hash || "";
+  const params = new URLSearchParams(hash.split("?")[1] || "");
+  const mode = params.get("mode");
+  if (mode) showForm(mode);
+})();
 
 function valid() {
-	const password = document.getElementById("pass").value;
-	const confirmPassword = document.getElementById("confirmPass").value;
-	const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&_])[A-Za-z\d@$!%*?&_]{8,}$/;
-	
-	if (!passwordRegex.test(password)) {
-		alert("Password must be at least 8 characters long and contain:\n- One uppercase letter\n- One lowercase letter\n- One number\n- One special character (@$!%*?&)");
-		return false;
-	}
-	
-	if (password !== confirmPassword) {
-		alert("Passwords do not match! Please make sure both password fields are identical.");
-		return false;
-	}
-	
-	return true;
+  const password = document.getElementById("pass").value;
+  const confirmPassword = document.getElementById("confirmPass").value;
+  const passwordRegex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&_])[A-Za-z\d@$!%*?&_]{8,}$/;
+
+  if (!passwordRegex.test(password)) {
+    alert(
+      "Password must be at least 8 characters long and contain:\n- One uppercase letter\n- One lowercase letter\n- One number\n- One special character (@$!%*?&)"
+    );
+    return false;
+  }
+
+  if (password !== confirmPassword) {
+    alert(
+      "Passwords do not match! Please make sure both password fields are identical."
+    );
+    return false;
+  }
+
+  return true;
 }
