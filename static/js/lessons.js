f0ecf3449dd99c9bd2046efd76c342a61da780f8
strat <strat@jamjar.ws>
2025-08-17 23:11:10 -0400

    fjewog

--- static/js/lessons.js
+++ static/js/lessons.js
@@ -86,7 +86,7 @@ async function searchLessonsByTopic(topic) {
 
    let results = await response.json();
    for (let i = 0; i < results.length; i++) {
-     results[i] = parseQuery(results[i]);
+     results[i] = parseQuery(results[i][0]);
    }
    return results;
  } catch (error) {
